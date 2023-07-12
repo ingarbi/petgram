6e78e3505199e2500f41c8d4f674ada5f7db90a6
ingarbi <ingarbi006@gmail.com>
2023-07-12 19:33:50 +0300

    24 commit

---
+++ backend/cats/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Achievement, AchievementCat, Cat
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'birth_year', 'owner', 'achievements')
+
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('name')
+
+
+class AchievementCatAdmin(admin.ModelAdmin):
+    list_display = ('achievement', 'cat')
+
+admin.site.register(Cat, CatAdmin)
+admin.site.register(Achievement, AchievementAdmin)
+admin.site.register(AchievementCat, AchievementCatAdmin)
