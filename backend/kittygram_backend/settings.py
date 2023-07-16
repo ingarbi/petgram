15cba31d25a71df8d55905bcc3d7a796971ef8de
ingarbi <ingarbi006@gmail.com>
2023-07-16 20:01:37 +0300

    last hope 18

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-123')
 
 DEBUG = os.getenv('DEBUG', default=True) == 'True'
 
-ALLOWED_HOSTS = ['mykittyapp.sytes.net', 'localhost', '127.0.0.1', '158.160.18.87']
+ALLOWED_HOSTS = ['mykittyapp.sytes.net', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
